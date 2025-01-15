890e2d55ce3017f3daecd24ad4476ef717c50f53
Vinicius Sonntag Dorow <vinicius.s.dorow@gmail.com>
2025-01-15 20:39:20 -0300

    feature: agriculture extractor is getting all the products

--- agricultura/agricultura_extract.py
+++ agricultura/agricultura_extract.py
@@ -2,47 +2,102 @@ import pandas as pd
 import sidrapy
 import numpy as np
 
-#https://apisidra.ibge.gov.br/home
+# https://apisidra.ibge.gov.br/home
 
-def get_data(variable,product,period) -> pd.DataFrame:
+
+def get_data(product) -> pd.DataFrame:
     data = sidrapy.get_table(
-        table_code = "5457",
-        territorial_level = "6",
-        ibge_territorial_code = "all",
-        variable = variable,
-        classifications = {"782":product},
-        period = period
+        table_code="5457",
+        territorial_level="6",
+        ibge_territorial_code="all",
+        variable="8331,216,214,112,215",
+        classifications={"782": product},
+        period="2023",
     )
     return data
 
+
 def get_all():
+    produtos = [
+        "40129",
+        "40092",
+        "45982",
+        "40329",
+        "40130",
+        "40099",
+        "40100",
+        "40101",
+        "40102",
+        "40103",
+        "40131",
+        "40136",
+        "40104",
+        "40105",
+        "40137",
+        "40468",
+        "40138",
+        "40139",
+        "40140",
+        "40141",
+        "40330",
+        "40106",
+        "40331",
+        "40142",
+        "40143",
+        "40107",
+        "40108",
+        "40109",
+        "40144",
+        "40145",
+        "40146",
+        "40147",
+        "40110",
+        "40111",
+        "40112",
+        "40148",
+        "40113",
+        "40114",
+        "40149",
+        "40150",
+        "40115",
+        "40151",
+        "40152",
+        "40116",
+        "40260",
+        "40117",
+        "40261",
+        "40118",
+        "40119",
+        "40262",
+        "40263",
+        "40264",
+        "40120",
+        "40121",
+        "40122",
+        "40265",
+        "40266",
+        "40267",
+        "40268",
+        "40269",
+        "40123",
+        "40270",
+        "40124",
+        "40125",
+        "40271",
+        "40126",
+        "40127",
+        "40128",
+        "40272",
+        "40273",
+        "40274",
+    ]
     df_full = pd.DataFrame()
-    #
-    for variavel in ("8331","216","214","215"):
-        for periodo in ("2022","2021"):
-            for produto in ("40129","40092"):
-                data = get_data(variavel,produto,periodo)
-                df_full = pd.concat([df_full,data])
-                print(f"{variavel},{produto},{periodo}")
+    for produto in produtos:
+        data = get_data(product=produto)
+        df_full = pd.concat([df_full, data])
+        print(f"{produto}")
     return df_full
+
 
 data = get_all()
 data
-
-def transform_data(df):
-    df.columns = df.iloc[0]
-    df = df.loc[df["Valor"]!="Valor"]
-    
-    df = ( df
-        .drop(columns=["Nível Territorial (Código)","Nível Territorial","Unidade de Medida (Código)","Produto das lavouras temporárias e permanentes","Ano (Código)","Variável"])
-        .assign(Valor = lambda x: x["Valor"].replace({"-":"0","...":"0"}))
-        .drop(columns = ["Unidade de Medida","Município"])
-        .rename(columns={"Município (Código)":"cod_municipio","Produto das lavouras temporárias e permanentes (Código)":"cod_produto","Ano":"ano","Variável (Código)":"cod_variavel","Valor":"valor"})
-        .astype({"valor":int,"cod_municipio":int,"cod_produto":int,"cod_variavel":int,"ano":int})
-    )
-    df = df.assign(valor = np.where(df["cod_variavel"] == 215, df["valor"] * 1000, df["valor"]))
-    return df
-
-df = transform_data(data)
-
-df
